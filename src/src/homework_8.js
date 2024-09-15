5cd286da82aaa9a5cd954e73d1738f2643848555
Beni3000 <Beni-3000@yandex.ru>
2024-09-15 22:34:28 +0300

    homework_8.2

--- src/src/homework_8.js
+++ src/src/homework_8.js
@@ -35,10 +35,69 @@ alert("Площадь круга равна: " + circle);
 */
 
 /*
-4 задание
+5 задание
 */
 /*
 let l = +prompt("Введите расстояние")
 let time = +prompt("Введите время")
 alert(l/time + "км/ч")
+*/
+
+/*
+6 задание
+*/
+/*
+const rate = Number(prompt("Введите текущий курс доллара по отношению к евро: "));
+let dollar = Number(prompt("Введите количество долларов для обмена: "));
+alert("По текущему курсу при обмене вы получите " + dollar * rate + " евро");
+*/
+
+/*
+7 задание
+*/
+/*
+const GB_MB = 1024;
+let flash_drive = Number(prompt("Каков объем вашей флешки в Гб? "));
+let size = Math.trunc(flash_drive * 1000 / GB_MB);
+if (size < 0) {
+  alert("Ваша флешка перезаполнена, освободите пространство");
+}
+else {
+  alert("На флешку поместится " + size + " файлов объемом 820 Мб");
+}
+*/
+
+/*
+8 задание
+*/
+/*
+let sum = Number(prompt("Cколько денег у вас есть? "));
+let price = Number(prompt("почем одна шоколадка? "));
+let sell = Math.trunc(sum / price);
+let change = sum - price * sell;
+if (sell < 1) {
+  alert("Сегодня вы на нуле. приходите в другой раз");
+}
+else {
+  alert("Получите свои " + sell + " шоколадки, а также сдачу " + (sum - price * sell) + " рублей ");
+}
+*/
+
+/*
+9 задание
+*/
+/*
+nmb = prompt("Введите трехзначное число: ");
+let a = Number(nmb % 10);
+let b = Number((nmb - a) / 10 % 10);
+let c = Number((nmb - a - b * 10) / 100);
+alert("Перевертыш: " + (a * 100 + b * 10 + c));
+*/
+
+/*
+10 задание
+*/
+/*
+let a = Number(prompt("Введите число для проверки на четность: "));
+alert((parseInt(a) % 2 == 0) ? "ЧЁТНОЕ" : "НЕЧЁТНОЕ");
 */
